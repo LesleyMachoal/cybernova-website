010700badc41f7edc44c24e397c92cd5737ee1d7
lesley machola <lesleymachola@gmail.com>
2026-05-14 19:25:25 +0200

    deployment

--- cybernova-website_Machola/src/main.jsx
+++ cybernova-website_Machola/src/main.jsx
@@ -6,10 +6,12 @@ import App from './App';
 import { store } from './store';
 import './index.css';
 
+const basename = import.meta.env.BASE_URL === '/' ? '/' : import.meta.env.BASE_URL.replace(/\/$/, '');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </Provider>
