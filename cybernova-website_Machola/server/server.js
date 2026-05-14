eb6a5714f11fda008eab0bd994d3139f584f4052
lesley machola <lesleymachola@gmail.com>
2026-05-14 23:56:28 +0200

    fix: bind server to 0.0.0.0 for Render port detection

--- cybernova-website_Machola/server/server.js
+++ cybernova-website_Machola/server/server.js
@@ -4,8 +4,9 @@ import app from './src/app.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5999;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+	console.log(`Server running on http://${HOST}:${PORT}`);
 });
 
